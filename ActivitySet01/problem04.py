b1dd4036e79cadc700824277dac9de3d3aa3a3f3
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-05-11 04:20:48 +0000

    nthg

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,9 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hr= int(input("Enter the no of hours"))
+nph= float(input("Enter pay per hour"))
+if hr<40:
+    pay=hr*nph
+else:
+    s=hr-40
+    pay=40*nph+s*1.5*nph
+print(pay)
+    
