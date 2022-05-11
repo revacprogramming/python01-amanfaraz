f60f76cfb9a4275995c83c7ada7195ee5db88fa4
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-05-11 04:32:59 +0000

    nthg

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,14 @@
-# Functions
-
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+grade=float(input("Enter the grade between 0.0 and 1.0"))
+if 0>grade>1:
+    print("ERROR")
+elif grade>=0.9:
+    print("A")
+elif grade>=0.8:
+    print("B")
+elif grade>=0.7:
+    print("C")
+elif grade>=0.6:
+    print("D")
+elif grade<0.6:
+    print("F")
+    
