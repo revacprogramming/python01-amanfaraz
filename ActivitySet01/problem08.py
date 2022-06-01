e31c6e62ea3d2c96b91bc68a5f9179c5976647c0
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-06-01 04:11:09 +0000

    nthg

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,19 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+h=input("Enter the file name:")
+try:
+    f=open(h)
+except:
+    print(h,"Not Found")
+count=0
+s=0
+for i in f:
+    if i.startswith('X-DSPAM-Confidence:'):
+        count=count+1
+        x=i.find('0')
+        l=i[x:]
+        c=float(l)
+        s=s+c
+    else:
+        continue
+average=s/count
+print('Average spam confidence:',average)
+ 
