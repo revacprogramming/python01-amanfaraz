8169467c192221eadb15ef4457ebd2c9f0f0863f
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-06-07 16:33:39 +0000

    doubt

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,24 @@
-# Dictionaries
+fname=input("Enter the file name:")
+a=[]
+count=0
+f=open(fname)
+try:
+    f="mbox-short.txt"
+except:
+    print("Invalid File")
+for lines in f:
+    if lines.startswith("From"):
+        b=lines.split()
+        a.append(b[1])
+        count+=1
+    else:
+        continue
+for i in a:
+    print(i)
+print("There were",count,"lines in the file with From as the first word")
 
-filename = "dataset/mbox-short.txt"
+    
+    
+     
+        
+    
