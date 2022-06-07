11f9e4e8d3746723e91c0b07d0c90bfcbefd9515
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-06-07 16:16:02 +0000

    nthg

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,16 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+a=[]
+try:
+    fname="romeo.txt"
+except:
+    print("Invalid File")
+f=open(fname)
+for lines in f:
+    b=lines.split()
+    for word in b:
+        if word in a:
+            continue
+        else:
+            a.append(word)
+a.sort()
+print(a)
